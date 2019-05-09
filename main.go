a370a8011259c9fd2770de3c4852a381b0a27f09
Charles Dixon <chvckd@gmail.com>
2019-05-09 10:08:52 +0100

    Add support for using n1ql instead of analytics

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"strings"
 
 	"gopkg.in/couchbase/gocb.v1"
@@ -26,13 +27,21 @@ type Result interface {
 }
 
 type couchbaseStore struct {
-	bucket  *gocb.Bucket
-	cluster *gocb.Cluster
+	bucket       *gocb.Bucket
+	cluster      *gocb.Cluster
+	useAnalytics bool
 }
 
 func (cs *couchbaseStore) Query(queryString string, params interface{}) (Result, error) {
-	query := gocb.NewAnalyticsQuery(queryString)
-	result, err := cs.bucket.ExecuteAnalyticsQuery(query, params)
+	var result Result
+	var err error
+	if cs.useAnalytics {
+		query := gocb.NewAnalyticsQuery(queryString)
+		result, err = cs.bucket.ExecuteAnalyticsQuery(query, params)
+	} else {
+		query := gocb.NewN1qlQuery(queryString)
+		result, err = cs.bucket.ExecuteN1qlQuery(query, params)
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -119,18 +128,68 @@ func main() {
 		log.Fatal(err)
 	}
 
+	populateQueries(options.BucketName)
+
 	store := couchbaseStore{bucket: bucket}
 
-	querySpanByTraceID = fmt.Sprintf(querySpanByTraceID, options.BucketName)
-	queryServiceNames = fmt.Sprintf(queryServiceNames, options.BucketName)
-	queryOperationNames = fmt.Sprintf(queryOperationNames, options.BucketName)
-	queryIDsByTag = fmt.Sprintf(queryIDsByTag, options.BucketName)
-	queryIDsByServiceName = fmt.Sprintf(queryIDsByServiceName, options.BucketName)
-	queryIDsByServiceAndOperationName = fmt.Sprintf(queryIDsByServiceAndOperationName, options.BucketName)
-	queryIDsByServiceAndOperationNameAndTags = fmt.Sprintf(queryIDsByServiceAndOperationNameAndTags, options.BucketName)
-	queryIDsByDuration = fmt.Sprintf(queryIDsByDuration, options.BucketName)
-
-	depsSelectStmt = fmt.Sprintf(depsSelectStmt, options.BucketName)
+	if options.UseAnalytics {
+		err := verifyAnalyticsSupported(options.ConnStr)
+		if err == nil {
+			store.useAnalytics = true
+		} else {
+			if options.UseN1QLFallback {
+				err := verifyN1QLSupported(options.ConnStr)
+				if err != nil {
+					log.Fatal("Neither analytics or N1QL available")
+				}
+			} else {
+				log.Fatal("Analytics not available")
+			}
+		}
+	} else {
+		err := verifyN1QLSupported(options.ConnStr)
+		if err != nil {
+			log.Fatal("N1QL not available")
+		}
+	}
 
 	grpc.Serve(&store)
+}
+
+func verifyServiceSupported(connStr, port, endpoint string) error {
+	splitConnStr := strings.Split(connStr, "://")
+	var conn string
+	if len(splitConnStr) > 1 {
+		conn = splitConnStr[1]
+	} else {
+		conn = splitConnStr[0]
+	}
+	_, err := http.Get(fmt.Sprintf("http://%s:%s/%s", conn, port, endpoint))
+	if err != nil {
+		return err
+	}
+
+	// the service exists so let's assume it's supported
+	return nil
+}
+
+func verifyAnalyticsSupported(connStr string) error {
+	return verifyServiceSupported(connStr, "8095", "/analytics/config/node")
+}
+
+func verifyN1QLSupported(connStr string) error {
+	return verifyServiceSupported(connStr, "8093", "/query/service")
+}
+
+func populateQueries(bucketName string) {
+	querySpanByTraceID = fmt.Sprintf(querySpanByTraceID, bucketName)
+	queryServiceNames = fmt.Sprintf(queryServiceNames, bucketName)
+	queryOperationNames = fmt.Sprintf(queryOperationNames, bucketName)
+	queryIDsByTag = fmt.Sprintf(queryIDsByTag, bucketName)
+	queryIDsByServiceName = fmt.Sprintf(queryIDsByServiceName, bucketName)
+	queryIDsByServiceAndOperationName = fmt.Sprintf(queryIDsByServiceAndOperationName, bucketName)
+	queryIDsByServiceAndOperationNameAndTags = fmt.Sprintf(queryIDsByServiceAndOperationNameAndTags, bucketName)
+	queryIDsByDuration = fmt.Sprintf(queryIDsByDuration, bucketName)
+
+	depsSelectStmt = fmt.Sprintf(depsSelectStmt, bucketName)
 }
--- options.go
+++ options.go
@@ -11,13 +11,17 @@ const username = "couchbase.username"
 const password = "couchbase.password"
 const connStr = "couchbase.connString"
 const useCertAuth = "couchbase.useCertAuth"
+const useAnalytics = "couchbase.useAnalytics"
+const n1qlFallback = "couchbase.n1qlFallback"
 
 type Options struct {
-	ConnStr     string
-	Username    string
-	Password    string
-	BucketName  string
-	UseCertAuth bool
+	ConnStr         string
+	Username        string
+	Password        string
+	BucketName      string
+	UseCertAuth     bool
+	UseAnalytics    bool
+	UseN1QLFallback bool
 }
 
 func (opt *Options) AddFlags(flagSet *flag.FlagSet) {
@@ -29,4 +33,6 @@ func (opt *Options) InitFromViper(v *viper.Viper) {
 	opt.Password = v.GetString(password)
 	opt.BucketName = v.GetString(bucketName)
 	opt.UseCertAuth = v.GetBool(useCertAuth)
+	opt.UseAnalytics = v.GetBool(useAnalytics)
+	opt.UseN1QLFallback = v.GetBool(n1qlFallback)
 }
